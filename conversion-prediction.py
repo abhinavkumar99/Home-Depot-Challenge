183cb066f97c795264eaf487f3bff8bbe4b89744
yatyrio <abhinavk99@gmail.com>
2017-11-05 10:28:17 -0500

    Added conversion prediction nn

--- conversion-prediction.py
+++ conversion-prediction.py
@@ -3,46 +3,59 @@ import tensorflow as tf
 from datetime import datetime as dt
 import numpy as np
 import os
+import matplotlib.pyplot as plt
+import sys
+import csv
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-data = json.loads(open("train.json").read().strip())
-train = []
-for line in data:
-    if list(line.values())[-1] == True:
-        train.append(np.array([1,0]))
-    else:
-        train.append(np.array([0,1]))
-print(len(train))
-data = [[list(row.values())[3] - list(row.values())[2]] + list(row.values())[4:9] + [list(row.values())[10]] for row in data]
-print(len(data))
-#print(data)
-data = np.array([np.array(i, dtype=np.int64) for i in data])
+def emb(file):
+    data = json.loads(open(str(file)).read().strip())
+    asdf = data[:]
+    train = []
+    for line in data:
+        if list(line.values())[-1] == True:
+            train.append(np.array([1,0]))
+        else:
+            train.append(np.array([0,1]))
+    data = [[list(row.values())[3] - list(row.values())[2]] + list(row.values())[4:9] + [list(row.values())[10]] for row in data]
+    dx = [list(row.values()) for row in asdf]
+
+    data = np.array([np.array(i, dtype=np.int64) for i in data])
+    
+    return data, train, dx
+
+data, train, _ = emb(sys.argv[1])
+x = np.array([i for i in range(len(data))])
+
+np.random.shuffle(x)
 minn = np.min(data, axis=0)
 maxx = np.max(data, axis=0)
-x = np.array([i for i in range(len(train))])
-np.random.shuffle(x)
-
 data = data[x]
 print(data.size)
 train = np.array(train)[x]
 data = (data-minn)/(maxx-minn)
-learning_rate = .001
-epochs = 450
-batch_size = 815
+
+learning_rate = .0005
+epochs = 1300
+#batch_size = 815
+batch_size = 8965
 train_len = 8965
 def NN(x, weights, biases):
     layer_1 = tf.nn.relu(tf.add(tf.matmul(x, weights["1"]), biases["1"]))
     layer_2 = tf.nn.relu(tf.add(tf.matmul(layer_1, weights["2"]), biases["2"]))
+    #layer_3 = tf.nn.relu(tf.add(tf.matmul(layer_2, weights["3"]), biases["3"]))
     out = tf.add(tf.matmul(layer_2, weights["out"]), biases["out"])
     return out
 
 weights = {
     "1": tf.Variable(tf.random_normal([7, 256])),
-    "2": tf.Variable(tf.random_normal([256, 128])),
-    "out": tf.Variable(tf.random_normal([128, 2]))
+    "2": tf.Variable(tf.random_normal([256, 200])),
+    # "3": tf.Variable(tf.random_normal([128,128])),
+    "out": tf.Variable(tf.random_normal([200, 2]))
 }
 biases = {
     "1": tf.Variable(tf.random_normal([256])),
-    "2": tf.Variable(tf.random_normal([128])),
+     "2": tf.Variable(tf.random_normal([200])),
+    # "3": tf.Variable(tf.random_normal([128])),
     "out": tf.Variable(tf.random_normal([2]))
 }
 x_vals = tf.placeholder(tf.float32, [None, 7])
@@ -54,6 +67,8 @@ is_correct = tf.equal(tf.argmax(pred, 1), tf.argmax(y_vals, 1))
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float64))
 init = tf.global_variables_initializer()
 with tf.Session() as sess:
+    c = []
+    a = []
     sess.run(init)
     testx = data[train_len:]
     testy = train[train_len:]
@@ -66,6 +81,8 @@ with tf.Session() as sess:
             trainy = train[batch*batch_size: (batch + 1)*batch_size]
 
             _, tcost, tacc = sess.run([optimizer, cost, accuracy], feed_dict={x_vals: trainx, y_vals: trainy})
+            c.append(tcost)
+            a.append(tacc)
             costs+=tcost
             accs+=tacc
         if epoch % 10 == 0:
@@ -80,3 +97,18 @@ with tf.Session() as sess:
             print(tecost)
             print(teacc)
             print("\n")
+
+    
+    plt.plot(range(len(a)), a)
+    plt.show()
+    plt.plot(range(len(c)), c)
+    plt.show()
+
+    y, _, dx = emb(sys.argv[2])
+    with open("out.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        count = 0
+        for i in y:
+            val = True if np.argmax(sess.run(tf.nn.softmax(pred), feed_dict={x_vals:[i]})) else False
+            writer.writerow([dx[count][0], val])
+            count+=1
